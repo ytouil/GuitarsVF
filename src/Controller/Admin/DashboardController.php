e66da7bcf4a9d1e0393cb8a22a6855a9f67533a8
ytouil <ytouil59@gmail.com>
2023-10-17 01:25:46 +0200

    adminCommit

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,34 +2,34 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Member;
+use App\Entity\Comment;
+use App\Entity\Gallery;
+use App\Entity\Guitar;
+use App\Entity\Inventory;
+use App\Entity\Message;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+    private $adminUrlGenerator;
+
+    public function __construct(AdminUrlGenerator $adminUrlGenerator)
+    {
+        $this->adminUrlGenerator = $adminUrlGenerator;
+    }
+    #[Route('/admin')]
     public function index(): Response
     {
-        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        $url = $this->adminUrlGenerator->setController(MemberCrudController::class)->generateUrl();
+        return $this->redirect($url);
+        // The rest of the commented code in this method is optional
+        // based on your requirements, so I'm keeping it as it is.
     }
 
     public function configureDashboard(): Dashboard
@@ -40,7 +40,16 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        return
+        [
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+            MenuItem::linkToCrud('Members', 'fa fa-users', Member::class),
+            MenuItem::linkToCrud('Comments', 'fa fa-comments', Comment::class),
+            MenuItem::linkToCrud('Galleries', 'fa fa-image', Gallery::class),
+            MenuItem::linkToCrud('Guitars', 'fa fa-guitar', Guitar::class),
+            MenuItem::linkToCrud('Inventories', 'fa fa-clipboard-list', Inventory::class),
+            MenuItem::linkToCrud('Messages', 'fa fa-envelope', Message::class),
+        ];
+         
     }
 }
