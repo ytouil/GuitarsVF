1da8d536f95d9c63fe2412c4fd38983e82c3e36e
ytouil <ytouil59@gmail.com>
2023-10-17 00:23:19 +0200

    fixtures update

--- src/Repository/Implementations/InventoryRepository.php
+++ src/Repository/Implementations/InventoryRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Inventory;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\Interfaces\InventoryRepositoryInterface;
 
 class InventoryRepository extends ServiceEntityRepository implements InventoryRepositoryInterface
 {
--- src/Repository/Implementations/MemberRepository.php
+++ src/Repository/Implementations/MemberRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Member;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\Interfaces\MemberRepositoryInterface;
 
 class MemberRepository extends ServiceEntityRepository implements MemberRepositoryInterface
 {
