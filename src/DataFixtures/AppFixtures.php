966f54c6f5ec45608402db39baeabe59f95b656d
Achrafvin <achraf.derkaoui@emsi-edu.ma>
2023-11-15 23:30:58 +0100

    Update5

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,174 +12,146 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class AppFixtures extends Fixture
 {
+    public function load(ObjectManager $manager)
+    {
+        $sourceDir = 'public/assets/dummyImages/';
+        $targetDir = 'public/assets/uploads/';
 
-public function load(ObjectManager $manager)
-{
-    $sourceDir = 'public/assets/dummyImages/';
-    $targetDir = 'public/assets/uploads/';
+        // Create Users
+        $admin = $this->createUser('admin@gmail.com', ['ROLE_ADMIN']);
+        $johnUser = $this->createUser('john@gmail.com', ['ROLE_USER']);
+        $aliceUser = $this->createUser('alice@gmail.com', ['ROLE_USER']);
 
-// Create Users
-    $admin = new User();
-    $admin->setEmail('admin@gmail.com');
-    $admin->setPassword(password_hash('123456', PASSWORD_DEFAULT));
-    $admin->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+        $manager->persist($johnUser);
+        $manager->persist($aliceUser);
 
-$johnUser = new User();
-$johnUser->setEmail('john@gmail.com');
-$johnUser->setPassword(password_hash('123456', PASSWORD_DEFAULT));
-$johnUser->setRoles(['ROLE_USER']);
+        // Create Members
+        $adminMember = $this->createMember('Admin', 'Bio about admin', $admin, '4.webp', $sourceDir, $targetDir);
+        $johnMember = $this->createMember('John Doe', 'Bio about John', $johnUser, '7.webp', $sourceDir, $targetDir);
+        $aliceMember = $this->createMember('Alice Smith', 'Bio about Alice', $aliceUser, '8.jpg', $sourceDir, $targetDir);
 
-$aliceUser = new User();
-$aliceUser->setEmail('alice@gmail.com');
-$aliceUser->setPassword(password_hash('123456', PASSWORD_DEFAULT));
-$aliceUser->setRoles(['ROLE_USER']);
+        $manager->persist($adminMember);
+        $manager->persist($johnMember);
+        $manager->persist($aliceMember);
 
-$manager->persist($admin);
-$manager->persist($johnUser);
-$manager->persist($aliceUser);
+        // Create Inventory
+        $johnInventory = $this->createInventory('John Inventory', $johnMember);
+        $aliceInventory = $this->createInventory('Alice Inventory', $aliceMember);
 
-// Create Members
-    $adminMember = new Member();
-    $adminMember->setFullName('Admin');
-    $adminMember->setBio('Bio about admin');
-    $adminMember->setUser($admin);
-    $imageName = '4.webp';
-    if (copy($sourceDir . $imageName, $targetDir . $imageName)) {
-        $file = new File($targetDir . $imageName);
-        $adminMember->setImage($file);
-        $adminMember->setImageName($imageName);
+        $manager->persist($johnInventory);
+        $manager->persist($aliceInventory);
+
+        // Create Guitars
+        $guitar1 = $this->createGuitar('Stratocaster', 'Description about Stratocaster', $johnInventory, '1.webp', $sourceDir, $targetDir);
+        $guitar2 = $this->createGuitar('Telecaster', 'Description about Telecaster', $aliceInventory, '2.webp', $sourceDir, $targetDir);
+
+        $manager->persist($guitar1);
+        $manager->persist($guitar2);
+
+        // Create Galleries
+        $johnGallery = $this->createGallery('John Gallery', 'Gallery belonging to John', $johnMember, [$guitar1], '9.webp', $sourceDir, $targetDir);
+        $aliceGallery = $this->createGallery('Alice Gallery', 'Gallery belonging to Alice', $aliceMember, [$guitar2], '10.webp', $sourceDir, $targetDir);
+
+        $manager->persist($johnGallery);
+        $manager->persist($aliceGallery);
+
+        // Create Comments
+        $comment1 = $this->createComment('Great guitar, John!', $guitar1, $johnMember);
+        $comment2 = $this->createComment('Nice guitar, Alice!', $guitar2, $aliceMember);
+
+        $manager->persist($comment1);
+        $manager->persist($comment2);
+
+        // Create Messages
+        $message1 = $this->createMessage('Hey John, how are you?', $johnMember, $aliceMember);
+        $message2 = $this->createMessage('Hey Alice, I am good!', $aliceMember, $johnMember);
+
+        $manager->persist($message1);
+        $manager->persist($message2);
+
+        $manager->flush();
     }
 
-
-$johnMember = new Member();
-$johnMember->setFullName('John Doe');
-$johnMember->setBio('Bio about John');
-$johnMember->setUser($johnUser);
-    $imageName = '7.webp';
-    if (copy($sourceDir . $imageName, $targetDir . $imageName)) {
-        $file = new File($targetDir . $imageName);
-        $adminMember->setImage($file);
-        $adminMember->setImageName($imageName);
+    private function createUser($email, $roles)
+    {
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword(password_hash('123456', PASSWORD_DEFAULT));
+        $user->setRoles($roles);
+        return $user;
     }
 
-$aliceMember = new Member();
-$aliceMember->setFullName('Alice Smith');
-$aliceMember->setBio('Bio about Alice');
-$aliceMember->setUser($aliceUser);
-    $imageName = '8.jpg';
-    if (copy($sourceDir . $imageName, $targetDir . $imageName)) {
-        $file = new File($targetDir . $imageName);
-        $adminMember->setImage($file);
-        $adminMember->setImageName($imageName);
+    private function createMember($fullName, $bio, $user, $imageName, $sourceDir, $targetDir)
+    {
+        $member = new Member();
+        $member->setFullName($fullName);
+        $member->setBio($bio);
+        $member->setUser($user);
+        $this->addImageToFile($member, $imageName, $sourceDir, $targetDir);
+        return $member;
     }
 
-$manager->persist($adminMember);
-$manager->persist($johnMember);
-$manager->persist($aliceMember);
-
-// Create Inventory
-
-$johnInventory = new Inventory();
-$johnInventory->setName('John Inventory');
-$johnInventory->setMember($johnMember);
-
-$aliceInventory = new Inventory();
-$aliceInventory->setName('Alice Inventory');
-$aliceInventory->setMember($aliceMember);
-
-$manager->persist($johnInventory);
-$manager->persist($aliceInventory);
-
-// Create Guitar
-$guitar1 = new Guitar();
-$guitar1->setModelName('Stratocaster');
-$guitar1->setDescription('Description about Stratocaster');
-$guitar1->setInventory($johnInventory);
-    $imageName = '1.webp';
-    if (copy($sourceDir . $imageName, $targetDir . $imageName)) {
-        $file = new File($targetDir . $imageName);
-        $adminMember->setImage($file);
-        $adminMember->setImageName($imageName);
+    private function createInventory($name, Member $member)
+    {
+        $inventory = new Inventory();
+        $inventory->setName($name);
+        $inventory->setMember($member);
+        return $inventory;
     }
 
-$guitar2 = new Guitar();
-$guitar2->setModelName('Telecaster');
-$guitar2->setDescription('Description about Telecaster');
-$guitar2->setInventory($aliceInventory);
-    $imageName = '2.webp';
-    if (copy($sourceDir . $imageName, $targetDir . $imageName)) {
-        $file = new File($targetDir . $imageName);
-        $adminMember->setImage($file);
-        $adminMember->setImageName($imageName);
+    private function createGuitar($modelName, $description, Inventory $inventory, $imageName, $sourceDir, $targetDir)
+    {
+        $guitar = new Guitar();
+        $guitar->setModelName($modelName);
+        $guitar->setDescription($description);
+        $guitar->setInventory($inventory);
+        $this->addImageToFile($guitar, $imageName, $sourceDir, $targetDir);
+        return $guitar;
     }
 
-$manager->persist($guitar1);
-$manager->persist($guitar2);
-
-// Create Gallery
-$johnGallery = new Gallery();
-$johnGallery->setName('John Gallery');
-$johnGallery->setDescription('Gallery belonging to John');
-$johnGallery->setMember($johnMember);
-$johnGallery->addGuitar($guitar1);
-    $imageName = '9.webp';
-    if (copy($sourceDir . $imageName, $targetDir . $imageName)) {
-        $file = new File($targetDir . $imageName);
-        $adminMember->setImage($file);
-        $adminMember->setImageName($imageName);
+    private function createGallery($name, $description, Member $member, array $guitars, $imageName, $sourceDir, $targetDir)
+    {
+        $gallery = new Gallery();
+        $gallery->setName($name);
+        $gallery->setDescription($description);
+        $gallery->setMember($member);
+        foreach ($guitars as $guitar) {
+            $gallery->addGuitar($guitar);
+        }
+        $this->addImageToFile($gallery, $imageName, $sourceDir, $targetDir);
+        return $gallery;
     }
 
-$aliceGallery = new Gallery();
-$aliceGallery->setName('Alice Gallery');
-$aliceGallery->setDescription('Gallery belonging to Alice');
-$aliceGallery->setMember($aliceMember);
-$aliceGallery->addGuitar($guitar2);
-    $imageName = '10.webp';
-    if (copy($sourceDir . $imageName, $targetDir . $imageName)) {
-        $file = new File($targetDir . $imageName);
-        $adminMember->setImage($file);
-        $adminMember->setImageName($imageName);
+    private function createComment($content, Guitar $guitar, Member $member)
+    {
+        $comment = new Comment();
+        $comment->setContent($content);
+        $comment->setGuitar($guitar);
+        $comment->setMember($member);
+        $comment->setTimestamp(new \DateTime());
+        return $comment;
     }
 
-$manager->persist($johnGallery);
-$manager->persist($aliceGallery);
+    private function createMessage($content, Member $sender, Member $receiver)
+    {
+        $message = new Message();
+        $message->setContent($content);
+        $message->setSender($sender);
+        $message->setReceiver($receiver);
+        $message->setTimestamp(new \DateTime());
+        return $message;
+    }
 
-// Create Comment
-$comment1 = new Comment();
-$comment1->setContent('Great guitar, John!');
-$comment1->setTimestamp(new \DateTime());
-$comment1->setGuitar($guitar1);
-$comment1->setMember($johnMember);
-
-$comment2 = new Comment();
-$comment2->setContent('Nice guitar, Alice!');
-$comment2->setTimestamp(new \DateTime());
-$comment2->setGuitar($guitar2);
-$comment2->setMember($aliceMember);
-
-$manager->persist($comment1);
-$manager->persist($comment2);
-
-// Create Message
-$message1 = new Message();
-$message1->setContent('Hey John, how are you?');
-$message1->setTimestamp(new \DateTime());
-$message1->setSender($aliceMember);
-$message1->setReceiver($johnMember);
-
-$message2 = new Message();
-$message2->setContent('Hey Alice, I am good!');
-$message2->setTimestamp(new \DateTime());
-$message2->setSender($johnMember);
-$message2->setReceiver($aliceMember);
-
-$manager->persist($message1);
-$manager->persist($message2);
-
-$manager->flush();
-}
+    private function addImageToFile($entity, $imageName, $sourceDir, $targetDir)
+    {
+        if (copy($sourceDir . $imageName, $targetDir . $imageName)) {
+            $file = new File($targetDir . $imageName);
+            $entity->setImage($file);
+            $entity->setImageName($imageName);
+        }
+    }
 }
